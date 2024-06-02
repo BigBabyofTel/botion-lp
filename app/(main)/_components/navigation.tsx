11c6125fa192500043583ecfab630b38b9ec624c
BigBabyofTel <augustus.tb@outlook.com>
2024-06-02 21:19:57 +0300

    user settings added

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserItem from "./user-item";
 
 export default function Navigation() {
   const pathname = usePathname();
@@ -112,7 +113,7 @@ export default function Navigation() {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>action items</p>
+          <UserItem /> 
         </div>
         <div className="mt-4">
           <p>documents</p>
